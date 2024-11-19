a89e044b908a3aa6e9673a2c6a13e2620d45fa45
Mario Avilés <4150123@alu.murciaeduca.es>
2024-11-19 09:30:24 +0100

    Solución del ejercicio hasta el index de proyectos

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProyectosController;
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [HomeController::class, 'getHome']);
 
 Route::get('login', function() {
     return view('auth.login');
@@ -14,17 +15,12 @@ Route::get('logout', function() {
     return 'Logout usuario';
 });
 
-Route::get('proyectos', function() {
-    return view('proyectos.index');
-});
+Route::get('proyectos', [ProyectosController::class, 'getIndex']);
 
-Route::get('proyectos/show/{id}', function($id) {
-    return view('proyectos.show', array('id'=>$id));
-})->where('id', '[0-9]+');
+Route::get('proyectos/show/{id}', [ProyectosController::class, 'getShow']
+)->where('id', '[0-9]+');
 
-Route::get('proyectos/create', function() {
-    return view('proyectos.create');
-});
+Route::get('proyectos/create', [ProyectosController::class, 'getCreate']);
 
 Route::get('proyectos/edit/{id}', function($id) {
     return view('proyectos.edit', array('id'=>$id));
@@ -33,3 +29,11 @@ Route::get('proyectos/edit/{id}', function($id) {
 Route::get('perfil/{id?}', function($id = null) {
     return $id ? 'Visualizar el currículo de '. $id : 'Visualizar el currículo propio';
 })->where('id', '[0-9]*');
+
+
+Route::get('proyectos/show/{id}', [ProyectosController::class, 'getShow']);
+
+
+Route::get('proyectos/edit{id}', [ProyectosController::class, 'getEdit']);
+
+
