19ebb6fdb054b72618149d74ef061920376ea267
Mario Avilés <4150123@alu.murciaeduca.es>
2024-12-03 09:53:17 +0100

    Columnas de Estudiantes y gestión de registros

---
+++ database/migrations/2024_12_03_074551_añadir_resto_atributos.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('estudiantes', function (Blueprint $table) {
+            $table->string('apellidos', 100)->nullable()->after('nombre');
+            $table->string('direccion', 200)->nullable()->after('apellidos');
+            $table->integer('votos')->nullable()->after('direccion');
+            $table->boolean('confirmado')->nullable()->after('votos');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('estudiantes', function (Blueprint $table) {
+            $table->dropColumn('apellidos');
+            $table->dropColumn('direccion');
+            $table->dropColumn('votos');
+            $table->dropColumn('confirmado');
+        });
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,43 @@ Route::get('perfil/{id?}', function($id = null) {
     return $id ? 'Visualizar el currículo de '. $id : 'Visualizar el currículo propio';
 })->where('id', '[0-9]*');
 
-Route::get('pruebaDB/{id}', function($id = null) {
-    $estudiantes = Estudiante::where('ciclo', 'C_'. $id)->get();
+Route::get('pruebaDB/{votos}', function($votos = null) {
+    $count = Estudiante::where('votos', '>', 100)->count();
+    $max = Estudiante::max('votos');
+    $min = Estudiante::min('votos');
+    $media = Estudiante::avg('votos');
+    $total = Estudiante::sum('votos');
+    $html = '<ul>';
+    $html .= '<li>Estudiantes con más de 100 votos: ' . $count. '</li>';
+    $html .= '<li>Máximo número de votos: ' . $max. '</li>';
+    $html .= '<li>Mínimo nñumero de votos: ' . $min. '</li>';
+    $html .= '<li>Media de votos: ' . $media. '</li>';
+    $html .= '<li>Total de votos: ' . $total . '</li>';
+    $html .= "</ul>\n<ul>";
+
+
+    $estudiantes = Estudiante::where('votos', '>', $votos)->take(5)->get();
     foreach ($estudiantes as $est) {
-            echo $est->nombre;
+            $html .= '<li>' . $est->nombre . '</li>';
     }
-});
+    $count = Estudiante::where('votos', '>', 100)->count();
+    $html .= 'Antes: ' . $count . '<br />';
+
+    $id = $votos ? $votos : 1;
+    $estudiante = Estudiante::findOrFail($id);
+    $estudiante->nombre = 'Juan';
+    $estudiante->apellidos = 'Martínez';
+    $estudiante->direccion = 'Dirección de Juan';
+    $estudiante->votos = 130;
+    $estudiante->confirmado = true;
+    $estudiante->ciclo = 'DAW';
+    $estudiante->save();
+
+    $count = Estudiante::where('votos', '>', 100)->count();
+    $html .= 'Después: ' . $count . '<br />';
+    return $html . '</ul>';
+
+    });
 
 
 
