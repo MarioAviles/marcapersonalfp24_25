8e4a6b0be29a760c48219f356a5098194184480f
Mario Avilés <4150123@alu.murciaeduca.es>
2025-01-23 08:46:36 +0100

    pruebas

--- app/Http/Middleware/ReactAdminResponse.php
+++ app/Http/Middleware/ReactAdminResponse.php
@@ -39,4 +39,20 @@ class ReactAdminResponse
         } catch (\Throwable $th) { }
         return $response;
     }
+
+    public static function applyFilter($request, $filterColumns)
+    {
+        $modelClassName = $request->route()->controller->modelclass;
+        $query = $modelClassName::query();
+
+        $filterValue = $request->q;
+        if ($filterValue) {
+            foreach ($filterColumns as $column) {
+                    $query->orWhere($column, 'like', '%' . $filterValue . '%');
+            }
+        }
+        return $query;
+    }
+
+    public static function applySort($query)
 }
